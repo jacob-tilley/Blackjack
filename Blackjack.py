60a891212e4186f3f830a616a46261ee1108af88
Jacob Tilley <jtilley7672@gmail.com>
2025-11-24 18:12:51 -0330

    Added fuctions in db.py to read and write to a text file. Added card suits, face values, and number values in seperate lists. Currently no functionality

--- Blackjack.py
+++ Blackjack.py
@@ -1,5 +1,13 @@
+import db
+FILENAME = "money.txt"
+
 def main():
-    pass
+    print("BLACKJACK!")
+    print("Blackjack payout is 3:2")
+
+    suits = ["Clubs", "Spades", "Diamonds", "Hearts"]
+    face_value = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
+    card_value = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 
 if __name__ == "__main__":
-    main
+    main()
--- db.py
+++ db.py
@@ -1,5 +1,19 @@
+FILENAME = "money.txt"
+
+def read_money():
+    with open(FILENAME, "r") as infile:
+        money = infile.readlines()
+        return int(money[0])
+    
+def write_money(money_amount):
+    with open(FILENAME, "w", newline="") as outfile:
+        outfile.write(str(money_amount))
+
 def main():
-    pass
+    money_amount = read_money()
+    print(money_amount)
+    money_amount -= 10
+    write_money(money_amount)
 
 if __name__ == "__main__":
-    main
+    main()
