e70918d70d6236f7444eb4121732f217df28c52c
Jacob Tilley <jtilley7672@gmail.com>
2025-11-28 12:08:20 -0330

    Moved betting amount from main to get_bet function.

--- Blackjack.py
+++ Blackjack.py
@@ -3,26 +3,25 @@ import random
 
 FILENAME = "money.txt"
 
-def create_deck(suits, ranks):
+def create_deck(suits, ranks, values):
     deck = []
     for suit in suits:
-        for rank in ranks:
-            deck.append(f"{rank} of {suit}")
+        for i, rank in enumerate(ranks):
+            value = values[i]
+            card = [suit, rank, value]
+            deck.append(card)
     return deck
 
-def deal_hand(deck):
+def deal_card(deck):
     hand = random.choice(deck)
     deck.remove(hand)
     return hand
 
-def main():
-    print("BLACKJACK!")
-    print("Blackjack payout is 3:2")
-    suits = ["Clubs", "Spades", "Diamonds", "Hearts"]
-    ranks = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
-    money = db.read_money()
+def hit(hand):
+    pass
+
+def get_bet(money):
     while True:
-        while True:
             try:
                 bet_amount = int(input("Bet amount (5-1000): "))
                 if bet_amount < 5 or bet_amount > 1000:
@@ -35,13 +34,24 @@ def main():
                     break
             except ValueError:
                 print("Invalid bet amount, try again.")
-        deck = create_deck(suits, ranks)
+    return bet_amount
+
+def main():
+    print("BLACKJACK!")
+    print("Blackjack payout is 3:2")
+    suits = ["Clubs", "Spades", "Diamonds", "Hearts"]
+    ranks = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
+    values = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    money = db.read_money()
+    while True:
+        bet = get_bet(money)
+        deck = create_deck(suits, ranks, values)
         player_hand = []
         dealer_hand = []
         for i in range(0, 2):
-            player_hand.append(deal_hand(deck))
+            player_hand.append(deal_card(deck))
         for i in range(0, 2):
-            dealer_hand.append(deal_hand(deck))
+            dealer_hand.append(deal_card(deck))
 
 if __name__ == "__main__":
     main()
